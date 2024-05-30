4aa8b5a8df1af0efe69d0ab524c75666132e53ff
Rasyaa21 <naufalrasya21907@gmail.com>
2024-05-30 15:45:22 +0700

    lgoin clear

--- app/src/main/java/com/rasya/contact_manager/LoginActivity.kt
+++ app/src/main/java/com/rasya/contact_manager/LoginActivity.kt
@@ -3,8 +3,13 @@ package com.rasya.contact_manager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import android.widget.Toast.LENGTH_SHORT
+import com.google.firebase.auth.FirebaseAuth
 import com.rasya.contact_manager.databinding.ActivityLoginBinding
 
+private lateinit var firebaseAuth: FirebaseAuth;
 
 class LoginActivity : AppCompatActivity() {
     @Suppress("DEPRECATION")
@@ -13,13 +18,38 @@ class LoginActivity : AppCompatActivity() {
         val binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
             overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
             finish()
+        }
+
+        binding.btLogin.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            if (email.isNotEmpty() && password.isNotEmpty()){
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener {
+                    if (it.isSuccessful){
+                        Toast.makeText(this, "Login Successfull", LENGTH_LONG).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        intent.putExtra("email", email)
+                        intent.putExtra("password", password)
+                        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+                        finish()
+                    }
+                    else{
+                        Toast.makeText(this,"Email Already Exist", LENGTH_LONG).show()
+                    }
+                }
+            }
+            else{
+                Toast.makeText(this, "please fill the form", LENGTH_LONG).show()
+            }
         }
     }
 }
