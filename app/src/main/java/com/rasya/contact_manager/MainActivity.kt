c692a5260bc60591845d0ecc6b3516157faa89c1
Rasyaa21 <naufalrasya21907@gmail.com>
2024-06-05 07:21:37 +0700

    main

--- app/src/main/java/com/rasya/contact_manager/MainActivity.kt
+++ app/src/main/java/com/rasya/contact_manager/MainActivity.kt
@@ -3,12 +3,14 @@ package com.rasya.contact_manager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
+import androidx.fragment.app.Fragment
 import com.rasya.contact_manager.databinding.ActivityMainBinding
 
 
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -16,8 +18,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.navbarHome,
-                R.id.navbarAccount
+                R.id.navbarHome -> {
+
+                }
+                R.id.navbarAccount -> {
+
+                }
+                R.id.navbarContact ->{
+
+                }
+                else -> false
             }
         }
 
@@ -26,5 +36,11 @@ class MainActivity : AppCompatActivity() {
         binding.tvEmail.text = email
 
 
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.,fragment)
+            commit()
     }
 }
