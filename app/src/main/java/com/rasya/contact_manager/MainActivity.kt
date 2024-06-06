b756917f2add564e6ba767d8b4f4ee91e863c7c2
Rasyaa21 <naufalrasya21907@gmail.com>
2024-06-06 10:16:56 +0700

    main

--- app/src/main/java/com/rasya/contact_manager/MainActivity.kt
+++ app/src/main/java/com/rasya/contact_manager/MainActivity.kt
@@ -2,34 +2,42 @@ package com.rasya.contact_manager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.view.WindowCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 import com.rasya.contact_manager.databinding.ActivityMainBinding
 
 
 private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        val navController = findNavController(R.id.fragmentContainer)
-
-        bottomNavigationView.setupWithNavController(navController)
-
+        replaceFragment(HomeActivity())
 
         val email = intent.getStringExtra("email")
-
         binding.tvEmail.text = email
 
+        binding.bottomNavigationView.setOnItemSelectedListener {
 
+            when(it.itemId){
+                R.id.HomeActivity -> replaceFragment(HomeActivity())
+                R.id.ContactActivity -> replaceFragment(ContactActivity())
+                R.id.AccountActivity -> replaceFragment(AccountActivity())
+
+                else -> {
+
+                }
+            }
+            true
+        }
     }
+
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
