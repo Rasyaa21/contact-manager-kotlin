0568431ea2c159d5f2ea411176ff124f2aaf0ffc
Rasyaa21 <naufalrasya21907@gmail.com>
2024-06-10 20:02:04 +0700

    dd

--- app/src/main/java/com/rasya/contact_manager/MainActivity.kt
+++ app/src/main/java/com/rasya/contact_manager/MainActivity.kt
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.replace
 import com.rasya.contact_manager.databinding.ActivityMainBinding
+import com.rasya.contact_manager.fragments.accountFragment
+import com.rasya.contact_manager.fragments.contactFragment
+import com.rasya.contact_manager.fragments.homeFragment
 
 
 private lateinit var binding: ActivityMainBinding
@@ -18,5 +22,32 @@ class MainActivity : AppCompatActivity() {
 
         val email = intent.getStringExtra("email")
         binding.tvEmail.text = email
+
+        val homeFragment = homeFragment()
+        val contactFragment = contactFragment()
+        val accountFragment = accountFragment()
+
+        setCurrentFragment(homeFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.HomeActivity->setCurrentFragment(homeFragment)
+                R.id.ContactActivity->setCurrentFragment(contactFragment)
+                R.id.AccountActivity->setCurrentFragment(accountFragment)
+
+            }
+            true
+        }
+
     }
+    private fun setCurrentFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
+    }
+
 }
+
+
+
---
+++ app/src/main/java/com/rasya/contact_manager/fragments/accountFragment.kt
@@ -0,0 +1,21 @@
+package com.rasya.contact_manager.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rasya.contact_manager.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+class accountFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+}
